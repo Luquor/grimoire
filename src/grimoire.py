0107095f05f985b5b53952573c80b94833907265
Lucas Signorelli <signorellilucas7@gmail.com>
2025-04-01 22:44:09 +0200

    feat(grimoire): add basic logging function

---
+++ src/grimoire.py
@@ -0,0 +1,31 @@
+from enum import Enum
+from datetime import datetime
+import inspect
+import os.path
+from dataclasses import dataclass
+
+class LogLevel(Enum):
+    info = 1
+    warn = 2
+    error = 3
+    debug = 4
+
+@dataclass
+class Logger:
+    def info(self, message: str = '') -> None:
+        print(generate_log(LogLevel.info, message))
+
+    def warn(self, message: str = '') -> None:
+        print(generate_log(LogLevel.warn, message))
+
+    def error(self, message: str = '') -> None:
+        print(generate_log(LogLevel.error, message))
+
+    def debug(self, message: str = '') -> None:
+        print(generate_log(LogLevel.debug, message))
+
+
+def generate_log(log_level: LogLevel, message: str) -> str:
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    log = f'{{"timestamp": "{timestamp}", "level": "{log_level.name}", "source": "{os.path.splitext(os.path.basename(inspect.stack()[1].filename))[0]}.py", "message": "{message}"}}'
+    return log
