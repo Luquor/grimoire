8dfedd20b1e079e0d3e5349acb924da2a4cf1728
Lucas Signorelli <signorellilucas7@gmail.com>
2025-05-04 17:54:22 +0200

    fix(grimoire)!: print line only for debug mode

--- src/grimoire.py
+++ src/grimoire.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from datetime import datetime
-from dataclasses import dataclass
 from inspect import getframeinfo
+from dataclasses import dataclass
 import inspect
 import os.path
 import sys
@@ -15,37 +15,32 @@ class LogLevel(Enum):
 
 @dataclass
 class Logger:
-    def info(self, message: str = '') -> 'LogWithLine':
+    def info(self, message: str = '') -> None:
         log_message = generate_log(LogLevel.info, message)
         print(log_message)
-        return LogWithLine(log_message)
 
-    def warn(self, message: str = '') -> 'LogWithLine':
+    def warn(self, message: str = '') -> None:
         log_message = generate_log(LogLevel.warn, message)
         print(log_message)
-        return LogWithLine(log_message)
 
-    def error(self, message: str = '') -> 'LogWithLine':
+    def error(self, message: str = '') -> None:
         log_message = generate_log(LogLevel.error, message)
-        print(log_message, file=sys.stderr)
-        return LogWithLine(log_message)
+        print(log_message)
 
-    def debug(self, message: str = '') -> 'LogWithLine':
+    def debug(self, message: str = '') -> None:
         log_message = generate_log(LogLevel.debug, message)
         print(log_message)
-        return LogWithLine(log_message)
-
-class LogWithLine:
-    def __init__(self, log_message: str):
-        self.log_message = log_message
-
-    def line(self) -> None:
-        caller = getframeinfo(inspect.stack()[1][0])
-        log_with_line = f'{self.log_message[:-1]}, "line": {caller.lineno}}}'
-        print(log_with_line)
-
 
 def generate_log(log_level: LogLevel, message: str) -> str:
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    log = f'{{"timestamp": "{timestamp}", "level": "{log_level.name}", "source": "{os.path.splitext(os.path.basename(inspect.stack()[2].filename))[0]}.py", "message": "{message}"}}'
+    caller_frame = inspect.stack()[2]
+    filename = os.path.basename(caller_frame.filename)
+    source = os.path.splitext(filename)[0]
+    
+    line_info = ""
+    if log_level == LogLevel.debug:
+        line_number = caller_frame.lineno
+        line_info = f", \"line\": {line_number}"
+    
+    log = f'{{"timestamp": "{timestamp}", "level": "{log_level.name}", "source": "{source}.py"{line_info}, "message": "{message}"}}'
     return log
