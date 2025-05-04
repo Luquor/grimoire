ef4bf73a8f55878c6e827a4f89fbfd5124770951
Lucas Signorelli <signorellilucas7@gmail.com>
2025-05-04 10:35:26 +0200

    feat(grimoire)!: change the class to be able to print the line of the call

--- src/grimoire.py
+++ src/grimoire.py
@@ -1,8 +1,9 @@
 from enum import Enum
 from datetime import datetime
+from dataclasses import dataclass
+from inspect import getframeinfo
 import inspect
 import os.path
-from dataclasses import dataclass
 import sys
 
 
@@ -14,17 +15,34 @@ class LogLevel(Enum):
 
 @dataclass
 class Logger:
-    def info(self, message: str = '') -> None:
-        print(generate_log(LogLevel.info, message))
+    def info(self, message: str = '') -> 'LogWithLine':
+        log_message = generate_log(LogLevel.info, message)
+        print(log_message)
+        return LogWithLine(log_message)
 
-    def warn(self, message: str = '') -> None:
-        print(generate_log(LogLevel.warn, message))
+    def warn(self, message: str = '') -> 'LogWithLine':
+        log_message = generate_log(LogLevel.warn, message)
+        print(log_message)
+        return LogWithLine(log_message)
 
-    def error(self, message: str = '') -> None:
-        print(generate_log(LogLevel.error, message), file=sys.stderr)
+    def error(self, message: str = '') -> 'LogWithLine':
+        log_message = generate_log(LogLevel.error, message)
+        print(log_message, file=sys.stderr)
+        return LogWithLine(log_message)
 
-    def debug(self, message: str = '') -> None:
-        print(generate_log(LogLevel.debug, message))
+    def debug(self, message: str = '') -> 'LogWithLine':
+        log_message = generate_log(LogLevel.debug, message)
+        print(log_message)
+        return LogWithLine(log_message)
+
+class LogWithLine:
+    def __init__(self, log_message: str):
+        self.log_message = log_message
+
+    def line(self) -> None:
+        caller = getframeinfo(inspect.stack()[1][0])
+        log_with_line = f'{self.log_message[:-1]}, "line": {caller.lineno}}}'
+        print(log_with_line)
 
 
 def generate_log(log_level: LogLevel, message: str) -> str:
